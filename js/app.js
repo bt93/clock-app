8ede8c478b6041087224ea8880bc920aa4f89c82
Jason Howie <howiejason93@gmail.com>
2019-01-08 17:14:45 -0500

    put date and time formating into a string

--- js/app.js
+++ js/app.js
@@ -2,33 +2,40 @@
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-// Gets current date and formats
-let d = new Date();
-let date = d.getDate();
-let month = months[d.getMonth()];
-let day = days[d.getDay()];
-let year = d.getFullYear();
-let hr = d.getHours();
-let min = d.getMinutes();
-let sec = d.getSeconds();
+function getTime() {
+	// Gets current date and formats
+	let d = new Date();
+	let date = d.getDate();
+	let month = months[d.getMonth()];
+	let day = days[d.getDay()];
+	let year = d.getFullYear();
+	let hr = d.getHours();
+	let min = d.getMinutes();
+	let sec = d.getSeconds();
+	let isAmPm = "AM";
+	let currentTime;
 
-let isAmPm = "AM";
-if (hr > 12) {
-	hr -= 12;
-	isAmPm = "PM";
+	if (hr > 12) {
+		hr -= 12;
+		isAmPm = "PM";
+	}
+
+	// Checks if hour is below 10 and adds zero
+	if (hr < 10) {
+		hr = '0' + hr;
+	}
+	// Does same but with minutes
+	if (min < 10) {
+		min = '0' + min;
+	}
+	// Does same but with seconds
+	if (sec < 10) {
+		sec = '0' + sec;
+	}
+
+	currentTime = `${day}, ${month} ${date}, ${year}\n${hr}:${min}:${sec} ${isAmPm}`;
+	return currentTime;
 }
 
-// Checks if hour is below 10 and adds zero
-if (hr < 10) {
-	hr = '0' + hr;
-}
-// Does same but with minutes
-if (min < 10) {
-	min = '0' + min;
-}
-// Does same but with seconds
-if (sec < 10) {
-	sec = '0' + sec;
-}
 
-console.log(day, month, date, year, hr, min, sec, isAmPm);
+console.log(getTime());
