ac36bd6965b555645f4c609af8d3c11b5d2c58fa
Jason Howie <howiejason93@gmail.com>
2019-01-08 16:13:58 -0500

    date formating

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
+// Months and days for easy access
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 // Gets current date and formats
 let d = new Date();
+let date = d.getDate();
 let month = months[d.getMonth()];
 let day = days[d.getDay()];
-let hr = d.getHours()
-console.log(month, day, hr);
+let year = d.getFullYear();
+let hr = d.getHours();
+let min = d.getMinutes();
+let sec = d.getSeconds();
+
+let isAmPm = "AM";
+if (hr > 12) {
+	hr -= 12;
+	isAmPm = "PM";
+}
+
+// Checks if hour is below 10 and adds zero
+if (hr < 10) {
+	hr = '0' + hr;
+}
+// Does same but with minutes
+if (min < 10) {
+	min = '0' + min;
+}
+// Does same but with seconds
+if (sec < 10) {
+	sec = '0' + sec;
+}
+
+console.log(day, month, date, year, hr, min, sec, isAmPm);
